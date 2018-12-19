ebfdc80eacd601372cf74a779ed01fb44241bdee
Andy Zhou <azhou4@stuy.edu>
2018-12-19 14:48:58 -0500

    started sending info

--- basic_server.c
+++ basic_server.c
@@ -7,4 +7,25 @@ int main() {
   int from_client;
 
   from_client = server_handshake( &to_client );
+
+  char send[100];
+  char receive[100];
+  while(1){
+    //Receiving Information
+    from_client = server_handshake( &to_client );
+    read(from_client, receive, sizeof(receive));
+    close(from_client);
+
+    //Modifies Information
+    for(int i = 0;receive[i];i++){
+      char c = receive[i] + 26;
+      if((c > 90 && c < 97) || (c > 123)){
+	c = c - 26;
+      }
+      send[i] = c;
+    }
+    
+    //Sends Changed Information
+    write(to_client, send, sizeof(send));
+  }
 }
--- client.c
+++ client.c
@@ -7,4 +7,20 @@ int main() {
   int from_server;
 
   from_server = client_handshake( &to_server );
+
+  char send[100];
+  char receive[100];
+  while(1){
+    //Sending Information
+    printf("%s\n", "input what you want to send");
+    scanf("%[^\n]s\n",send);
+    write(to_server, send, sizeof(send));
+
+    //Receives Changed Information
+    from_server = client_handshake( &to_server );
+    read(from_server, receive, sizeof(receive));
+    close(from_server);
+
+    printf("Message received : %s\n", receive);
+  }
 }
