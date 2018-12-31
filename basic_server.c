42e2dfbf6eda5ebb055b9c84c30324f66a66b8bb
0AndyZhou0 <azhou4@stuy.edu>
2018-12-31 11:16:43 -0500

    added forking

--- basic_server.c
+++ basic_server.c
@@ -12,28 +12,32 @@ int main() {
   //char * receive = malloc(sizeof(char*));
   while(1){
     from_client = server_handshake( &to_client );
+
+    if(fork() == 0){
+      //Receiving Information
+      while(read(from_client, receive, sizeof(receive)) > 0){
+	receive[strlen(receive)] = '\0';
+	printf("recieved info : %s\n", receive);
     
-    //Receiving Information
-    while(read(from_client, receive, sizeof(receive)) > 0){
-      receive[strlen(receive)] = '\0';
-      printf("recieved info : %s\n", receive);
-    
-      //Modifies Information
-      for(int i = 0;receive[i];i++){
-        int c = receive[i];
-	if((c >= 65 && c <= 77) || (c >= 97 && c <= 109)){
-	  c+= 13;
-	}else if((c >= 78 && c <= 90) || (c >= 110 && c <= 122)){
-	  c -= 13;
+	//Modifies Information
+	for(int i = 0;receive[i];i++){
+	  int c = receive[i];
+	  if((c >= 65 && c <= 77) || (c >= 97 && c <= 109)){
+	    c+= 13;
+	  }else if((c >= 78 && c <= 90) || (c >= 110 && c <= 122)){
+	    c -= 13;
+	  }
+	  send[i] = c;
 	}
-	send[i] = c;
-      }
-      //printf("modified info : %s\n", receive);
+	//printf("modified info : %s\n", receive);
     
-      //Sends Changed Information
-      write(to_client, send, sizeof(send));
+	//Sends Changed Information
+	write(to_client, send, sizeof(send));
+      }
+      printf("error %d\n", errno);
+    }else{
+
     }
-    printf("error %d\n", errno);
   }
   return 0;
 }
